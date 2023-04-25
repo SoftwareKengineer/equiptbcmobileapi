a0631d63b9cd4ea4afdc4c2727fa8818a64fe65b
Birante SY <birantesy@gmail.com>
2023-04-25 01:13:48 +0000

    Add database ✅

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('equiptbcmobileapi', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database:', error);
+});
+
 const app = express()
 
 app.get('/', function (req, res) {
